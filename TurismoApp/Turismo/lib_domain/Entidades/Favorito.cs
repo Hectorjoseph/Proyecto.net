8785f17a525be0b8c0c17cfa63d40362a9cdea4e
HJoseph <hjosephnavarro882@gmail.com>
2025-04-02 00:21:01 -0500

    Add files via upload

---
+++ TurismoApp/Turismo/lib_domain/Entidades/Favorito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_dominio.Entidades
+{
+    public class Favorito
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La fecha de agregado es obligatoria")]
+        public DateTime FechaAgregado { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+
+        [ForeignKey("Lugar")]
+        public int LugarId { get; set; }
+
+        public Lugar? Lugar { get; set; }
+    }
+}
---
+++ TurismoApp/Turismo/lib_domain/Entidades/Ruta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic; // Para ICollection
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // Para ForeignKey
+
+namespace lib_dominio.Entidades
+{
+    public class Ruta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El origen es obligatorio")]
+        [MaxLength(100)]
+        public required string Origen { get; set; }
+
+        [Required(ErrorMessage = "El destino es obligatorio")]
+        [MaxLength(100)]
+        public required string Destino { get; set; }
+
+        [Required(ErrorMessage = "La distancia es obligatoria")]
+        public double Distancia { get; set; }
+
+        [Required(ErrorMessage = "El tiempo estimado es obligatorio")]
+        public double TiempoEstimado { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+
+        // Relación con PuntoRuta
+        public ICollection<PuntoRuta> PuntosRuta { get; set; } = new List<PuntoRuta>();
+    }
+}
